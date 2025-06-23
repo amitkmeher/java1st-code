ddd63351e36816e16f587056a2469f66bf943faf
amitkmeher <amitkmeher@gmail.com>
2025-06-24 01:36:44 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World! this is my 1st java code");
+        System.out.println("Hello, World! this is my 2nd java code. please find mistakes");
     }
 }
