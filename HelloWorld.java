f225b3454a4b6545169c4d682718b28bef40c28e
amitkmeher <amitkmeher@gmail.com>
2025-06-24 04:48:28 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class Simple {
     public static void main(String[] args) {
         System.out.println("Hello, World! this is my 1st java code");
-        System.out.println("Hello, World! this is my 2nd java code. please find the mistakes");
+        System.out.println("Hello, World! this is my 2nd java code with docker. please find the mistakes");
        }
 }
